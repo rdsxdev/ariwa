d8a8534bce334a2946164a36cb80baa920854d52
Tanishq Kaushal <tanishqkrk@gmail.com>
2026-02-17 17:45:45 +0530

    Updates

--- components/Keyboard.tsx
+++ components/Keyboard.tsx
@@ -14,37 +14,55 @@ export function Keyboard({
   addLetter: (letter: string) => void;
   letterStatus: { letter: string; status: string }[];
 }) {
-  // console.log(letterStatus);
+  console.log(letterStatus);
 
   return (
     <div className=" min-h-36 min-w-lg flex-col space-y-1">
       <div className="flex gap-1 justify-center items-center ">
         {qwertyLettersRow1.map((letter) => {
+          let status = letterStatus
+            .filter((x) => x.letter === letter.toUpperCase())
+            .map((x) => x.status);
+
+          const statusToPass = status.includes("CORRECT")
+            ? "CORRECT"
+            : status.includes("EXISTS")
+              ? "EXISTS"
+              : status.includes("INCORRECT")
+                ? "INCORRECT"
+                : "";
+          console.log(statusToPass);
           return (
             <Key
               addLetter={addLetter}
               key={letter}
               letter={letter}
               lastPressedKey={lastPressedKey}
-              status={
-                letterStatus.filter((x) => x.letter === letter.toUpperCase())[0]
-                  ?.status
-              }
+              status={statusToPass}
             />
           );
         })}
       </div>
       <div className="flex gap-1 justify-center items-center">
         {qwertyLettersRow2.map((letter) => {
+          let status = letterStatus
+            .filter((x) => x.letter === letter.toUpperCase())
+            .map((x) => x.status);
+          const statusToPass = status.includes("CORRECT")
+            ? "CORRECT"
+            : status.includes("EXISTS")
+              ? "EXISTS"
+              : status.includes("INCORRECT")
+                ? "INCORRECT"
+                : "";
+
           return (
             <Key
               addLetter={addLetter}
               key={letter}
               letter={letter}
               lastPressedKey={lastPressedKey}
-              status={
-                letterStatus.filter((x) => x.letter === letter)[0]?.status
-              }
+              status={statusToPass}
             />
           );
         })}
@@ -57,15 +75,24 @@ export function Keyboard({
           lastPressedKey={lastPressedKey}
         />
         {qwertyLettersRow3.map((letter) => {
+          let status = letterStatus
+            .filter((x) => x.letter === letter.toUpperCase())
+            .map((x) => x.status);
+          const statusToPass = status.includes("CORRECT")
+            ? "CORRECT"
+            : status.includes("EXISTS")
+              ? "EXISTS"
+              : status.includes("INCORRECT")
+                ? "INCORRECT"
+                : "";
+
           return (
             <Key
               addLetter={addLetter}
               key={letter}
               letter={letter}
               lastPressedKey={lastPressedKey}
-              status={
-                letterStatus.filter((x) => x.letter === letter)[0]?.status
-              }
+              status={statusToPass}
             />
           );
         })}
@@ -91,7 +118,7 @@ export function Key({
   addLetter: (letter: string) => void;
   status?: string;
 }) {
-  console.log(status);
+  console.log(letter.toUpperCase(), status);
   return (
     <motion.div
       key={letter}
@@ -106,21 +133,21 @@ export function Key({
       animate={{
         scale: lastPressedKey?.toLowerCase() === letter.toLowerCase() ? 0.8 : 1,
       }}
-      className={`p-3 py-3   min-w-12 w-fit   flex justify-center items-center bg-foreground/10 rounded-md uppercase font-semibold cursor-pointer select-none duration-150 border 
+      className={`p-3 py-3   min-w-12 w-fit   flex justify-center items-center  rounded-md uppercase font-semibold cursor-pointer select-none duration-150 border 
 ${
   lastPressedKey?.toLowerCase() === letter.toLowerCase()
     ? "border-foreground/60 bg-foreground/40"
-    : "border-background bg-foreground/10"
+    : "border-background "
 }
                     
 ${
   status === "CORRECT"
-    ? "bg-correct"
+    ? "bg-correct text-white"
     : status === "EXISTS"
       ? "bg-incorrect text-background"
       : status === "INCORRECT"
         ? "bg-foreground/50 text-background"
-        : ""
+        : "bg-foreground/10"
 }
 `}
     >
